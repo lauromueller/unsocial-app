52c2fcb4ddd424a28b90cc85a7108ea240601513
Lauro Fialho Müller <lauro.muller@lfmuller.com>
2021-01-16 12:49:28 +0100

    feat: add web-client project (#48)

---
+++ web-client/webpack.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    index: "./src/index.tsx"
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ]
+}
+
+if (isProd) {
+  config.optimization = {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+    stats: "errors-only",
+    overlay: true,
+  }
+}
+
+module.exports = config;
