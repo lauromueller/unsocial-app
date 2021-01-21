c03449b7b7298d95cc850e114f8a41ccba588c9e
Lauro Fialho Müller <lauro.muller@lfmuller.com>
2021-01-21 18:18:11 +0100

    feat(web-client): add jest, testing library, and cypress (#68)

---
+++ web-client/src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('should find the corresponding component', () => {
+  render(<App />);
+  expect(screen.getByText(/unsocial/i)).toBeDefined();
+})
