e359931eea9371a9bc7a5b736c08b258d4a5302c
Lauro Fialho Müller <lauro.muller@lfmuller.com>
2021-02-05 22:13:06 +0100

    chore(web-client): integrate material ui + adjust webpack and jest (#110)

--- web-client/jest.config.js
+++ web-client/jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   roots: [
     "<rootDir>/src"
-  ]
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/file-mock.ts",
+    "\\.(css|less)$": "<rootDir>/src/__mocks__/style-mock.ts"
+  }
 };
